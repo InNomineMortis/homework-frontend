e31adbcd810ca84507490a09d56a5ca5c98271c9
Dmitry <dmpuchenkov@gmail.com>
2019-09-02 15:55:49 +0300

    Объединил условия, написал функцию на reduce, исправил на void (0) undefined

--- source/get.js
+++ source/get.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const get = (object, field) => {
-    if (typeof object !== "object") return undefined;
-    if (object === null) return undefined;
-    if (typeof field !== "string") return undefined;
+    if (typeof object !== "object" || object === null  || typeof field !== "string")
+        return void (0);
     field = field.slice(1);
     let fields = field.split('.');
     if (field === '') {
@@ -11,10 +10,12 @@ const get = (object, field) => {
     }
     if (fields[0] in object) {
         let newObject = object;
-        fields.forEach(value => {
-            newObject = newObject[value];
-        });
-        return newObject
+        // fields.forEach(value => {
+        //     newObject = newObject[value];
+        // });
+        return fields.reduce((previousValue, currentValue) => {
+            return previousValue[currentValue];
+        }, newObject);
     }
-    return undefined
+    return void (0);
 };
