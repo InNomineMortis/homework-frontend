0b2cc606794d75e5cb09e901e2ecd6fde11a6213
Dmitry <dmpuchenkov@gmail.com>
2019-09-02 18:03:41 +0300

    Small fix

--- source/get.js
+++ source/get.js
@@ -1,21 +1,17 @@
 'use strict';
 
 const get = (object, field) => {
-    if (typeof object !== "object" || object === null  || typeof field !== "string")
+    if ((typeof object !== "object") || (object === null)  || (typeof field !== "string")) {
         return void (0);
-    field = field.slice(1);
-    let fields = field.split('.');
-    if (field === '') {
-        return object
+    }
+    let fields = field.slice(1).split('.');
+    if (field === '.') {
+        return object;
     }
     if (fields[0] in object) {
-        let newObject = object;
-        // fields.forEach(value => {
-        //     newObject = newObject[value];
-        // });
         return fields.reduce((previousValue, currentValue) => {
             return previousValue[currentValue];
-        }, newObject);
+        }, object);
     }
     return void (0);
 };
